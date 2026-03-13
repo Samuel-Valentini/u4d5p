d8410983946560aa9a41959890c477bfb26e8bf6
Samuel-Valentini <samuelvitalia@gmail.com>
2026-03-13 13:40:14 +0100

    case "a" done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import player.multimediaelement.MultimediaElement;
+import player.multimediaelement.playable.Audio;
 
 import java.util.Scanner;
 
@@ -29,13 +30,42 @@ public class Main {
         MultimediaElement[] arrayOfMultimediaElement = new MultimediaElement[arrayLength];
 
         for (int i = 0; i < arrayLength; i++) {
+
+            System.out.println("Perfetto, inseriamo l'elemento n. " + i);
+
             String fileType;
 
             do {
                 System.out.println("Digita 'a' per caricare un audio, 'v' per caricare un video, 'i' per caricare un'immagine");
                 fileType = scanner.nextLine();
             } while (!(fileType.equals("a") || fileType.equals("v") || fileType.equals("i")));
-            
+
+            String title;
+            int duration;
+            int volume;
+            Audio audio;
+
+
+            switch (fileType) {
+                case "a": {
+                    System.out.println("Inserisci il titolo dell'audio:");
+                    title = scanner.nextLine();
+                    System.out.println("Inserisci la durata come numero intero");
+                    duration = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Inserisci il volume con un numero intero da 0 a 100");
+                    volume = (Math.min(Math.abs(Integer.parseInt(scanner.nextLine())), 100));
+                    audio = new Audio(title, duration, volume);
+                    arrayOfMultimediaElement[i] = audio;
+                    break;
+                }
+
+                case "v": {
+                    break;
+                }
+                case "i": {
+                    break;
+                }
+            }
 
         }
 
